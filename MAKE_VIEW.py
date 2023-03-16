c879b9c381011bf5438fd7749736b100cf7b94be
HBenpitsu <hbenpitsu15@gmail.com>
2023-03-17 02:36:08 +0900

    ON THE WAY2 I DON'T KNOW WHY THOUGH, TABLE CAN'T BE SHOWN.

--- MAKE_VIEW.py
+++ MAKE_VIEW.py
@@ -1,6 +1,20 @@
 import os
 import glob
 import json
+import collections
+
+HTMLSRC="""
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="./VIEW.css">
+	</head>
+	<body>
+		{}
+	</body>
+</html>
+""".replace("\t","").replace("\n","")
+AUTO_SETTING=True
 
 comparisons = []
 
@@ -52,34 +66,83 @@ def show():
 
 def make():
 	global comparisons
+	print("----make----")
 	json_loder = make_json_loader()
-	structure,order,omitted = integrate_json(json_loder)
+	structure,omitted = integrate_json(json_loder)
+	structure = integrate_omitted_elements(structure,omitted)
+	table_src = generate_table(structure)
+	htm = HTMLSRC.format(table_src).replace("\n","<br>").replace("\t","    ")
+	if AUTO_SETTING:
+		name = "VIEW"
+	else:
+		name = input("save as...:")
+	with open(f"./{name}.html","w",encoding="utf-8") as f:
+		f.write(htm)
+	print("check VIEW.html")
+	print("----make----")
+	print("done.")
+
+def integrate_omitted_elements(structure,omitted):
+	new_keys = set()
+	for omitted_infomations in omitted.values():
+		new_keys.add(*omitted_infomations.keys())
+	for new_key in new_keys:
+		structure[new_key] = []
+		for item in structure["ITEM_ID"]:
+			if not item in omitted:
+				structure[new_key].append(None)
+			elif not new_key in omitted[item]:
+				structure[new_key].append(None)
+			else:
+				structure[new_key].append(omitted[item][new_key])
+	return structure
+def generate_tr(td_list,header=None):
+	result = "<tr>"
+	if header is not None:
+		result += "<th class=\"{}\">{}</th>".format(header,header)
+	toggle = False
+	for td in td_list:
+		if toggle:
+			result += "<td class=\"{} even\">{}</td>".format(header,td)
+		else:
+			result += "<td class=\"{} odd\">{}</td>".format(header,td)
+		toggle = not toggle
+	result += "</tr>"
+	return result
+def generate_table(structure:dict):
+	result="<talbe><tbody>"
+	for header in structure:
+		result += generate_tr(structure[header],header)
+	result += "</tbody></table>"
+	return result
 
 def integrate_json(json_loder):
 	global comparisons
-	item_order=[]
-	first_item,first_item_name = next(json_loder)
-	item_order.append(first_item_name)
-	structure = {row_label:[row_value] for row_label,row_value in first_item.items()}
+	first_item = next(json_loder)
+	structure=collections.OrderedDict()
+	for row_label,row_value in first_item.items():
+		structure[row_label]=[row_value] 
+	structure.move_to_end("ITEM_ID",last=False)
 	omitted_elements={}
-	for item,item_name in json_loder:
-		item_order.append(item_name)
+	for item in json_loder:
 		key_differ = set(item.keys()) - set(structure.keys())
 		if key_differ:
-			omitted_elements[item_name]={key:item[key] for key in key_differ}
+			omitted_elements[item["ITEM_ID"]]={key:item[key] for key in key_differ}
 		for row_label in structure.keys():
 			if row_label in item:
 				structure[row_label].append(item[row_label])
 			else:
 				structure[row_label].append(None)
-	return structure,item_order,omitted_elements
+	return structure,omitted_elements
 
 def make_json_loader():
 	for item in comparisons:
 		with open(item,"r") as opened_file:
 			loaded_json = json.load(opened_file)
-		yield loaded_json,item
-
+		for key in [unpack for unpack in loaded_json.keys()]:
+			loaded_json[key.replace(" ","_")] = loaded_json.pop(key)
+		loaded_json["ITEM_ID"] = item.split("/")[-1].split(".")[0]
+		yield loaded_json
 def call_events(inp):
 	if inp == "a":
 		add()
@@ -94,4 +157,8 @@ def call_events(inp):
 	else:
 		print("unknown command.")
 
-main()
+if AUTO_SETTING:
+	comparisons += glob.glob("./Comparision/*")
+	make()
+else:
+	main()
